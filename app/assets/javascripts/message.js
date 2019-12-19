47b0d35e27d9db65569306223d514f672f907425
h-takuya21 <h.t02011989@gmail.com>
2019-12-20 02:13:31 +0900

    Revert "修正修正、確認しました"
    This reverts commit b679d47f2f904c90be40fa7f68f9ad1bae002504.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,6 +35,7 @@ $(function(){
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
+    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('create')
     
